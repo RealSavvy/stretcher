4b210c6a16535d278f4600efa8f70bf95c79130b
Matt Swezey <mattswezey@me.com>
2016-07-05 14:09:38 -0500

    Removed: Faraday.load_autoloaded_constants

--- lib/stretcher.rb
+++ lib/stretcher.rb
@@ -5,7 +5,6 @@ require 'excon'
 require 'faraday'
 require 'faraday_middleware'
 require 'faraday_middleware/multi_json'
-Faraday.load_autoloaded_constants
 require "stretcher/version"
 require 'stretcher/request_error'
 require 'stretcher/search_results'
